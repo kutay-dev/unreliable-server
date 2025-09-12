99bb12eacd983371fd91a383d91fa7d4e0b4e131
Kutay Cereci <kutay.crc42@gmail.com>
2025-09-12 23:01:47 +0300

    fix imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from './core/prisma/prisma.module';
-import { ChatModule } from './modules/chat/chat.module';
-import { ChatGateway } from './modules/chat/chat.gateway';
-import { ChatService } from './modules/chat/chat.service';
-import { UserService } from './modules/user/user.service';
-import { UserController } from './modules/user/user.controller';
-import { UserModule } from './modules/user/user.module';
+import { PrismaModule } from '@/core/prisma/prisma.module';
+import { ChatModule } from '@/modules/chat/chat.module';
+import { ChatGateway } from '@/modules/chat/chat.gateway';
+import { ChatService } from '@/modules/chat/chat.service';
+import { UserService } from '@/modules/user/user.service';
+import { UserController } from '@/modules/user/user.controller';
+import { UserModule } from '@/modules/user/user.module';
 
 @Module({
   imports: [
